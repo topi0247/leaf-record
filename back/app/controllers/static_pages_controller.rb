2d39d6ca10f1aaf3b5854440873abbbea5661974
Kanae-Nishina <karubona.ra247@gmail.com>
2024-04-24 14:40:30 +0900

    デプロイ確認用にAPI側にルートパス追加

---
+++ back/app/controllers/static_pages_controller.rb
@@ -0,0 +1,5 @@
+class StaticPagesController < ApplicationController
+  def index
+    render json: { message: 'Hello World' }
+  end
+end
--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "static_pages#index"
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {
     omniauth_callbacks: 'auth/omniauth_callbacks',
   }
