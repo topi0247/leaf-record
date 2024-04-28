d8a8189aca8cfdac5f0733ba0a41a1fc13edef15
Kanae-Nishina <karubona.ra247@gmail.com>
2024-04-28 22:38:40 +0900

    PWA化

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+import withPWAInit from "@ducanh2912/next-pwa";
+const withPWA = withPWAInit({
+  dest: "public",
+});
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA(nextConfig);
--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { MainLayout } from "@/components/layouts";
@@ -8,9 +8,44 @@ import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const APP_NAME = "Leaf Record ～大草原不可避～";
+const APP_DEFAULT_TITLE = "Leaf Record ～大草原不可避～";
+const APP_TITLE_TEMPLATE = "%s - Leaf Record ～大草原不可避～";
+const APP_DESCRIPTION = "GitHubが大草原に！草が生やせる記録アプリ！！";
+
 export const metadata: Metadata = {
-  title: "Leaf Record",
-  description: "GitHub記録アプリ",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 };
 
 export default function RootLayout({
